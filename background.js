d85f1da5b7535f7e0d48e8e364d6f6bcb9b7498f
templateK <templateK@github.com>
2017-05-18 13:25:38 +0900

    es6 refactoring

--- background.js
+++ background.js
@@ -1,138 +1,77 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2017 jo-taro. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// A generic onclick callback function.
+var tabLocations = new Map();
 
-var store= {
-    windowId : -1,
-    tabId : -1,
-    tabIndex : -1
-};
+function makeTabLocation (windowId, tab) {
+    return 
+}
 
-async function genericOnClick(info, tab) {
-//   console.log("item " + info.menuItemId + " was clicked");
-//   console.log("info: " + JSON.stringify(info));
-//   console.log("tab: " + JSON.stringify(tab));
-//   let moveProperties = {
-//   };
-//   var moveComplete = await chrome.tabs.move(tab.id, moveProperties);
-    var cstore={
-        windowId : -1,
-        tabId : -1,
-        tabIndex : -1
-    };
+async function onClick(info, tab) {
     try {
-        var queryInfo = {};
-        var queryResults = await chrome.tabs.query(queryInfo)
-        for(let i=0; i<queryResults.length; i++) {
-            if (queryResults[i].id == tab.id) {
-                cstore.windowId = queryResults[i].windowId;
-                cstore.tabIndex = queryResults[i].index;
-                cstore.tabId    = tab.id;
-                break;
-            }
-        }
+        const currentWindow = await chrome.windows.getCurrent();
+        const winId = currentWindow.id;
         
-        var getWindowInfo = { windowTypes : ["normal"] }
-        var currentTabWindow = await chrome.windows.get(cstore.windowId, getWindowInfo);
-        console.log("window");
-        console.log(currentTabWindow);
-        
-        if ( currentTabWindow.state == "fullscreen" ) {
-            if (  store.windowId === -1) return;
-            let updateInfo = {state: "normal"};
-            var normalTabWindow = await chrome.windows.update(currentTabWindow.id, updateInfo);
-
-            let moveProperties = {"windowId": store.windowId, "index": store.tabIndex};
-            var movedTab = await chrome.tabs.move(tab.id, moveProperties);
-            var updateProperties = {active: true};
-            chrome.tabs.update(movedTab.id, updateProperties);
-            
-            console.log("normal screen");
-            store.windowId = -1;
-            store.tabId = -1;
-            store.tabId = -1;
-            store.tabIndex  = -1;
-            console.log("store value");
-            console.log(store);
+        if ( tabLocations.has(winId) ) {
+            doFullscreen(winId, tab.id);
         } else {
-            store = cstore;
-            console.log("store value");
-            console.log(store);
-            let createData = {
-                "tabId": tab.id,
-                "type": "normal",
-                "state": "fullscreen"
-            };
-            chrome.windows.create(createData);
-            console.log("full screen");
+            doNormalScreen(winId, tab.id);
         } 
+
     } catch (error) {
         console.log(error);
     }
 };
-                              
 
-
-// Create one test item for each context type.
-// var contexts = ["page","selection","link","editable","image","video",
-//                 "audio"];
-var contexts = ["all"];
-for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-//   var title = "Test '" + context + "' menu item";
-  var title = "전체 화면"
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": genericOnClick});
-
-  console.log("'" + context + "' item:" + id);
+function makeTabLocation(winId, tabIndex) {
+    return { windowId : winId, tabIndex : tabIndex};
 }
 
+async function doNormalScreen(winId, tabId) {
+    
+    // retreive current tab
+    const currentTab = await chrome.tabs.get(tabId);
 
-// Create a parent item and two children.
-// var parent = chrome.contextMenus.create({"title": "Test parent item"});
-// var child1 = chrome.contextMenus.create(
-//   {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
-// var child2 = chrome.contextMenus.create(
-//   {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
-// console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
+    // make new window with current tab
+    const createData = {
+        tabId : currentTab.id,
+        type  : "normal",
+        state : "fullscreen"
+    };
+    const fullscreenWindow = await chrome.windows.create(createData);
 
+    // store previous window,tabindex of tab of fullscreen window
+    const tabLocation  = makeTabLocation(winId, currentTab.index);
+    tabLocations.set( fullscreenWindow.id, tabLocation);
 
-// Create some radio items.
-// function radioOnClick(info, tab) {
-//   console.log("radio item " + info.menuItemId +
-//               " was clicked (previous checked state was "  +
-//               info.wasChecked + ")");
-// }
-// var radio1 = chrome.contextMenus.create({"title": "Radio 1", "type": "radio",
-//                                          "onclick":radioOnClick});
-// var radio2 = chrome.contextMenus.create({"title": "Radio 2", "type": "radio",
-//                                          "onclick":radioOnClick});
-// console.log("radio1:" + radio1 + " radio2:" + radio2);
+    console.log("full screen");
+}
 
+async function doFullscreen(winId, tabId) {
+    
+    // make window of current tab normal because
+    // chrome.tabs.move only works in normal window mode
+    const updateInfo = {state: "normal"};
+    const normalTabWindow = await chrome.windows.update(winId, updateInfo);
+    
+    // after window state is normal, delete stored tab location
+    // because current window is no longer in fullscreen
+    const tabLocation = tabLocations.get(winId);
+    tabLocations.delete(winId);
 
-// Create some checkbox items.
-// function checkboxOnClick(info, tab) {
-//   console.log(JSON.stringify(info));
-//   console.log("checkbox item " + info.menuItemId +
-//               " was clicked, state is now: " + info.checked +
-//               "(previous state was " + info.wasChecked + ")");
+    // move current tab to original position of original window 
+    // NOTE: original window may be changed, so position can be incorrect
+    const moveProperties = { windowId : tabLocation.windowId, index: tabLocation.tabIndex };
+    const movedTab = await chrome.tabs.move(tabId, moveProperties);
+    
+    // finally make moved tab active
+    const updateProperties = {active: true};
+    chrome.tabs.update(movedTab.id, updateProperties);
 
-// }
-// var checkbox1 = chrome.contextMenus.create(
-//   {"title": "Checkbox1", "type": "checkbox", "onclick":checkboxOnClick});
-// var checkbox2 = chrome.contextMenus.create(
-//   {"title": "Checkbox2", "type": "checkbox", "onclick":checkboxOnClick});
-// console.log("checkbox1:" + checkbox1 + " checkbox2:" + checkbox2);
+    console.log("normal screen");
+}
 
-
-// Intentionally create an invalid item, to show off error checking in the
-// create callback.
-// console.log("About to try creating an invalid item - an error about " +
-//             "item 999 should show up");
-// chrome.contextMenus.create({"title": "Oops", "parentId":999}, function() {
-//   if (chrome.extension.lastError) {
-//     console.log("Got expected error: " + chrome.extension.lastError.message);
-//   }
-// });                            
+const title = "전체 화면"
+const createInfo = {"title": title, "contexts":["all"], "onclick": onClick};
+chrome.contextMenus.create(createInfo);
