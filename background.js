b6c0c7686b4f494e72c996b73f939f0a00faf4ae
templateK <templateK@github.com>
2017-05-18 13:41:35 +0900

    revert tab behavior in inconsistent state

--- background.js
+++ background.js
@@ -15,15 +15,9 @@ async function onClick(info, tab) {
         const isFullscreen = currentWindow.state === "fullscreen";
         
         if ( tabLocations.has(winId) ) {
-            if ( isFullscreen ) {
-                doNormalScreen(winId, tab.id);
-            } else {
-                // this window is already in normal mode, 
-                // but required to be in normal, so delete inconsistent information
-                tabLocations.delete(winId);
-            }
+            doNormalScreen(winId, tab.id, tabLocations.get(winId));
         } else {
-            doFullscreen(winId, tab.id);
+            doFullScreen(winId, tab.id);
         } 
 
     } catch (error) {
@@ -49,24 +43,19 @@ async function doFullScreen(winId, tabId) {
     const fullscreenWindow = await chrome.windows.create(createData);
 
     // store previous window,tabindex of tab of fullscreen window
-    const tabLocation  = makeTabLocation(winId, currentTab.index);
+    const tabLocation = makeTabLocation(winId, currentTab.index);
     tabLocations.set( fullscreenWindow.id, tabLocation);
 
     console.log("full screen");
 }
 
-async function doNormalscreen(winId, tabId) {
+async function doNormalScreen(winId, tabId, tabLocation) {
     
     // make window of current tab normal because
     // chrome.tabs.move only works in normal window mode
     const updateInfo = {state: "normal"};
     const normalTabWindow = await chrome.windows.update(winId, updateInfo);
     
-    // after window state is normal, delete stored tab location
-    // because current window is no longer in fullscreen
-    const tabLocation = tabLocations.get(winId);
-    tabLocations.delete(winId);
-
     // move current tab to original position of original window 
     // NOTE: original window may be changed, so position can be incorrect
     const moveProperties = { windowId : tabLocation.windowId, index: tabLocation.tabIndex };
@@ -76,9 +65,12 @@ async function doNormalscreen(winId, tabId) {
     const updateProperties = {active: true};
     chrome.tabs.update(movedTab.id, updateProperties);
 
+    // after all movement is completed, delete stored tab location
+    tabLocations.delete(winId);
+
     console.log("normal screen");
 }
 
-const title = "전체 화면"
+const title = "전체 화면 토글"
 const createInfo = {"title": title, "contexts":["all"], "onclick": onClick};
 chrome.contextMenus.create(createInfo);
